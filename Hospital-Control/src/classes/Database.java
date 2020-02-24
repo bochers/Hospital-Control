350d7d05292702ac4559e56a463304258d4babe0
isaacfulcrum <isaac_cas_2000@outlook.com>
2020-02-24 13:39:19 -0600

    Updated Database

--- Hospital-Control/src/classes/Database.java
+++ Hospital-Control/src/classes/Database.java
@@ -13,14 +13,33 @@ import java.util.*;
  * @author isaac
  */
 public class Database {
-    
-    public Database()
-    {
-        ArrayList<Patient> patients;
+
+    ArrayList<Patient> patients;
+    ArrayList<Employee> employees;
+    DataInputStream inputFile;
+    DataOutputStream outputFile;
+
+    public Database() {
         patients = new ArrayList<>();
-        
+        employees = new ArrayList<>();
+    }
+
+    public void newPatient(Patient p) 
+    {    
+        patients.add(p);
     }
     
-    
-    
+    public Patient searchPatient(int id) 
+    {    
+        Patient c = new Patient();
+        
+        for(Patient p: patients)
+        {
+            if(p.getID().equals(id))
+            {
+                return p;
+            }
+        }
+        return c;
+    }
 }
